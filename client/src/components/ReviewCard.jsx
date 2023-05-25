20a9663fd13c5ec4eff1b6735595e290eff28a50
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-25 16:47:34 -0700

    feat: remove review

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,17 +1,47 @@
 import {
   Avatar,
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardHeader,
-  IconButton,
+  Modal,
   Typography,
 } from "@mui/material";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { red } from "@mui/material/colors";
 
-export const ReviewCard = () => {
+import { red } from "@mui/material/colors";
+import PropTypes from "prop-types";
+import { useContext, useState } from "react";
+import { UserContext } from "../App";
+import { EditReviewForm } from "./EditReviewForm";
+
+export const ReviewCard = ({
+  id,
+  userName,
+  review,
+  productId,
+  fetchAllReviews,
+}) => {
+  const { user } = useContext(UserContext);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
+  const handleRemoveReview = async () => {
+    const response = await fetch(`/api/reviews/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      console.log("Success: Review removed");
+      fetchAllReviews(productId);
+    } else {
+      console.error("Error:", response.statusText);
+    }
+  };
+
   return (
     <Card sx={{ maxWidth: 500 }}>
       <CardHeader
@@ -20,26 +50,68 @@ export const ReviewCard = () => {
             R
           </Avatar>
         }
-        title="Shrimp and Chorizo Paella"
+        title={userName}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {review}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
-          edit
-        </Button>
-        <Button size="small" color="primary">
-          remove
-        </Button>
-        <IconButton aria-label="add to favorites">
+        {userName === user ? (
+          <Button size="small" color="primary" onClick={handleOpenModal}>
+            edit
+          </Button>
+        ) : (
+          ""
+        )}
+
+        {userName === user ? (
+          <Button size="small" color="primary" onClick={handleRemoveReview}>
+            remove
+          </Button>
+        ) : (
+          ""
+        )}
+        {/* <IconButton aria-label="add to favorites">
           <FavoriteIcon />
-        </IconButton>
+        </IconButton> */}
       </CardActions>
+      <Modal
+        open={modalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <EditReviewForm
+            reviewId={id}
+            name={userName}
+            originalReview={review}
+            productId={productId}
+            fetchAllReviews={fetchAllReviews}
+          />
+        </Box>
+      </Modal>
     </Card>
   );
+};
+
+ReviewCard.propTypes = {
+  id: PropTypes.string,
+  userName: PropTypes.string,
+  review: PropTypes.string,
+  productId: PropTypes.string,
+  fetchAllReviews: PropTypes.func,
 };
