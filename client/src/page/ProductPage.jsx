2d14269631e8d7d656ec9128aba54b794c9699cd
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-25 16:43:28 -0700

    feat: get all reviews

--- client/src/page/ProductPage.jsx
+++ client/src/page/ProductPage.jsx
@@ -1,26 +1,35 @@
 import { Box, Button, Modal } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ReviewForm } from "../components/ReviewForm";
 import { ReviewCard } from "../components/ReviewCard";
+import { UserContext } from "../App";
 
 export const ProductPage = () => {
   const [product, setProduct] = useState({});
+  const [reviews, setReviews] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
+  const { user } = useContext(UserContext);
   let { id } = useParams();
   const productId = id;
 
   useEffect(() => {
     fetchProductsDataById(productId);
-  }, []);
+    fetchAllReviews(productId);
+  }, [productId]);
 
   const fetchProductsDataById = async (productId) => {
     const res = await fetch(
       `http://makeup-api.herokuapp.com/api/v1/products/${productId}.json`
     );
-
     const data = await res.json();
     setProduct(data);
+  };
+
+  const fetchAllReviews = async (productId) => {
+    const res = await fetch(`/api/reviews/${productId}`);
+    const data = await res.json();
+    setReviews(data);
   };
 
   const handleOpenModal = () => setModalOpen(true);
@@ -31,9 +40,15 @@ export const ProductPage = () => {
       <img src={product.image_link} alt={product.name} />
       <div>{product.name}</div>
       <div>{product.description}</div>
-      <Button variant="outlined" onClick={handleOpenModal}>
-        Create
-      </Button>
+
+      {user && !reviews.find((item) => item.user === user) ? (
+        <Button variant="outlined" onClick={handleOpenModal}>
+          Create
+        </Button>
+      ) : (
+        ""
+      )}
+
       <Modal
         open={modalOpen}
         onClose={handleCloseModal}
@@ -52,10 +67,20 @@ export const ProductPage = () => {
             p: 4,
           }}
         >
-          <ReviewForm />
+          <ReviewForm productId={productId} fetchAllReviews={fetchAllReviews} />
         </Box>
       </Modal>
-      <ReviewCard />
+
+      {reviews.map((item, index) => (
+        <ReviewCard
+          id={item._id}
+          userName={item.user}
+          review={item.review}
+          key={index}
+          productId={productId}
+          fetchAllReviews={fetchAllReviews}
+        />
+      ))}
     </>
   );
 };
