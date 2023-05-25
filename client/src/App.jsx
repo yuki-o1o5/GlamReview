b128c1936ef9a6ef5ae66159df4da71a9302b514
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-25 16:40:30 -0700

    feat: contextapi and useReducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,38 @@ import { Header } from "./components/Header";
 import { ProductPage } from "./page/ProductPage";
 import { LoginPage } from "./page/LoginPage";
 import { SignupPage } from "./page/SignupPage";
+import { createContext, useReducer } from "react";
+
+export const UserContext = createContext();
+
+const initialState = {
+  user: null,
+};
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(userReducer, initialState);
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<ProductPage />} />
-      </Routes>
+      <UserContext.Provider value={{ user: state.user, dispatch }}>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<ProductPage />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
