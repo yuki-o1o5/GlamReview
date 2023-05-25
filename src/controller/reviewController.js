973f5425863f16cf98cd6064327a3978d3d2f04a
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-25 16:36:11 -0700

    docs: change review backend

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -2,9 +2,57 @@ const Review = require("../model/reviewModel");
 
 class ReviewController {
   async createReview(req, res) {
-    const review = new Review(req.body);
-    const result = await review.save();
-    res.send(result);
+    try {
+      const review = new Review(req.body);
+      const result = await review.save();
+      res.send(result);
+    } catch {
+      res.status(500).send({ error: error.message });
+    }
+  }
+
+  async getReviewsByProduct(req, res) {
+    try {
+      const { productId } = req.params;
+      const reviews = await Review.find({ productId: productId });
+      if (reviews) {
+        res.json(reviews);
+      } else {
+        res.status(404).send({ error: "No reviews found for this product" });
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+
+  async updateReview(req, res) {
+    try {
+      const { id } = req.params;
+      const review = await Review.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      if (review) {
+        res.json(review);
+      } else {
+        res.status(404).send({ error: "Review not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+
+  async deleteReview(req, res) {
+    try {
+      const { id } = req.params;
+      const review = await Review.findByIdAndRemove(id);
+      if (review) {
+        res.json({ message: "Review deleted successfully" });
+      } else {
+        res.status(404).send({ error: "Review not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
   }
 }
 
--- src/model/reviewModel.js
+++ src/model/reviewModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-  productId: Number,
+  productId: String,
   user: String,
   review: String,
 });
