d546be35b4dbf22e0948d28c5828aa03afb464ed
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-25 16:34:55 -0700

    docs: remove comment backend

--- src/model/commentModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CommentSchema = new mongoose.Schema({
-  reviewId: Number,
-  user: String,
-  comment: String,
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
