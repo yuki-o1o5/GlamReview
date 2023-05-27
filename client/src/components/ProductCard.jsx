18a2010e9c6ac2009bf9114c836a1a041da10f07
yuki-o1o5 <jkksst1712@gmail.com>
2023-05-27 14:53:14 -0700

    docs: add detail from api

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,6 +1,13 @@
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Rating,
+  Typography,
+} from "@mui/material";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export const ProductCard = ({ url, title, id, price, rating }) => {
   const navigate = useNavigate();
@@ -10,19 +17,26 @@ export const ProductCard = ({ url, title, id, price, rating }) => {
   };
 
   return (
-    <Card sx={{ maxWidth: 200 }}>
+    <Card style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <CardActionArea onClick={() => handleClick(id)}>
-        <img src={url} alt={title} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <ImageContainer>
+          <img src={url} alt={title} />
+        </ImageContainer>
+        <CardContent
+          style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+        >
+          <Title gutterBottom variant="h6" component="div">
             {title}
-          </Typography>
+          </Title>
           <Typography gutterBottom variant="h5" component="div">
             {price}
           </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {rating}
-          </Typography>
+          <ReviewContainer>
+            <ReviewNumber gutterBottom variant="h6" component="div">
+              {rating ? `${rating}` : `0`}
+            </ReviewNumber>
+            <Rating name="read-only" value={rating} readOnly />
+          </ReviewContainer>
         </CardContent>
       </CardActionArea>
     </Card>
@@ -36,3 +50,26 @@ ProductCard.propTypes = {
   price: PropTypes.string,
   rating: PropTypes.number,
 };
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled(Typography)`
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const ReviewNumber = styled(Typography)`
+  margin-right: 10px;
+`;
+
+const ReviewContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
--- client/src/page/ProductPage.jsx
+++ client/src/page/ProductPage.jsx
@@ -1,4 +1,5 @@
-import { Box, Button, Modal } from "@mui/material";
+import { Box, Button, Modal, Rating, Typography } from "@mui/material";
+import styled from "styled-components";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ReviewForm } from "../components/ReviewForm";
@@ -23,12 +24,14 @@ export const ProductPage = () => {
       `http://makeup-api.herokuapp.com/api/v1/products/${productId}.json`
     );
     const data = await res.json();
+    console.log(data, "data");
     setProduct(data);
   };
 
   const fetchAllReviews = async (productId) => {
     const res = await fetch(`/api/reviews/${productId}`);
     const data = await res.json();
+
     setReviews(data);
   };
 
@@ -36,51 +39,115 @@ export const ProductPage = () => {
   const handleCloseModal = () => setModalOpen(false);
 
   return (
-    <>
-      <img src={product.image_link} alt={product.name} />
-      <div>{product.name}</div>
-      <div>{product.description}</div>
-
-      {user && !reviews.find((item) => item.user === user) ? (
-        <Button variant="outlined" onClick={handleOpenModal}>
-          Create
-        </Button>
-      ) : (
-        ""
-      )}
-
-      <Modal
-        open={modalOpen}
-        onClose={handleCloseModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <ReviewForm productId={productId} fetchAllReviews={fetchAllReviews} />
-        </Box>
-      </Modal>
-
-      {reviews.map((item, index) => (
-        <ReviewCard
-          id={item._id}
-          userName={item.user}
-          review={item.review}
-          key={index}
-          productId={productId}
-          fetchAllReviews={fetchAllReviews}
-        />
-      ))}
-    </>
+    <PageContainer>
+      <ProductContainer>
+        <ImageContainer>
+          <img src={product.image_link} alt={product.name} />
+        </ImageContainer>
+        <TextsContainer>
+          <Typography gutterBottom variant="h6" component="div">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.price}
+          </Typography>
+          <ReviewContainer>
+            <ReviewNumber gutterBottom variant="h6" component="div">
+              {product.rating ? `${product.rating}` : `0 review`}
+            </ReviewNumber>
+            <Rating name="read-only" value={product.rating || 0} readOnly />
+          </ReviewContainer>
+          <Typography gutterBottom component="div">
+            {product.description}
+          </Typography>
+          <ButtonContainer>
+            {user && !reviews.find((item) => item.user === user) ? (
+              <Button variant="outlined" onClick={handleOpenModal}>
+                Create
+              </Button>
+            ) : (
+              ""
+            )}
+          </ButtonContainer>
+        </TextsContainer>
+      </ProductContainer>
+      <AllReviewsWrapper>
+        <Modal open={modalOpen} onClose={handleCloseModal}>
+          <Box
+            sx={{
+              width: "450px",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 7,
+            }}
+          >
+            <ReviewForm
+              productId={productId}
+              fetchAllReviews={fetchAllReviews}
+            />
+          </Box>
+        </Modal>
+        {reviews.map((item, index) => (
+          <ReviewCard
+            id={item._id}
+            userName={item.user}
+            review={item.review}
+            key={index}
+            productId={productId}
+            fetchAllReviews={fetchAllReviews}
+          />
+        ))}
+      </AllReviewsWrapper>
+    </PageContainer>
   );
 };
+
+const PageContainer = styled.div`
+  padding: 10px 120px;
+`;
+
+const ProductContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 50px 0 50px;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+`;
+
+const TextsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 60%;
+`;
+
+const ReviewNumber = styled(Typography)`
+  margin-right: 10px;
+`;
+
+const ReviewContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AllReviewsWrapper = styled.div`
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: flex-end;
+`;
