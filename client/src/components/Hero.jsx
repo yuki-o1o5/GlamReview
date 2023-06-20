4560db9880e69ee5c49511b4312254ebefd17ac1
yuki-o1o5 <jkksst1712@gmail.com>
2023-06-20 13:31:50 -0700

    docs: add img

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,10 @@
+import TopImage from "../assets/moreno-matkovic-kpWUwelRlJM-unsplash.jpg";
+
 export const Hero = () => {
   return (
     <div style={{ display: "flex", width: "100%", height: "100%" }}>
       <img
-        src="https://plus.unsplash.com/premium_photo-1661284864840-7d9539d4b34a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
+        src={TopImage}
         alt="eye"
         height={500}
         style={{ objectFit: "cover" }}
