47e185e1435b0700c0436281dd80e157e1e147a1
yuki-o1o5 <jkksst1712@gmail.com>
2023-06-27 18:00:06 -0700

    docs: add fields in the review collections

--- src/model/reviewModel.js
+++ src/model/reviewModel.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const ReviewSchema = new mongoose.Schema({
   productId: String,
   user: String,
+  title: String,
   review: String,
+  score: Number,
+  date: String,
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
