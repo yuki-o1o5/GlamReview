d52a6fcfe5b37e4c0534f6c22e99060f05399893
yuki-o1o5 <jkksst1712@gmail.com>
2023-07-05 13:55:41 -0700

    docs: change fetchProductsByBrandName func

--- client/src/utils/fetchApiUtils.js
+++ client/src/utils/fetchApiUtils.js
@@ -1,11 +1,21 @@
-export const fetchProductsByBrandName = async (brandName) => {
-  try {
-    const res = await fetch(
+export const fetchProductsByBrandName = (brandName) =>
+  new Promise((resolve, reject) => {
+    fetch(
       `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandName}`
-    );
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.err(err);
-  }
-};
+    )
+      .then((response) => response.json())
+      .then((result) => resolve(result))
+      .catch((err) => reject(err));
+  });
+
+// export const fetchProductsByBrandName = async (brandName) => {
+//   try {
+//     const res = await fetch(
+//       `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandName}`
+//     );
+//     const data = await res.json();
+//     return data;
+//   } catch (err) {
+//     console.err(err);
+//   }
+// };
