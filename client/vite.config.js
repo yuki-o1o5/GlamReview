94e66a20e43d51f71500a3fb026240b009c13307
yuki-o1o5 <jkksst1712@gmail.com>
2023-07-13 12:14:52 -0700

    config: docs: change path again

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // outDir: "build"
-    rollupOptions: {
-      external: ["/public/hero.jpg"],
-    },
-  },
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
